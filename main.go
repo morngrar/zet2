d240601168f9819b7bef10d8212943b48bded65d
Svein-Kåre Bjørnsen <github@sbjornsen.no>
2025-02-02 15:01:48 +0100

    feat: implements branch command

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -19,6 +22,7 @@ var defaultPrefix = "tmp"
 // prefixes that are disallowed because they will come in conflict with
 // subcommands
 var reservedPrefixes = []string{
+	"branch",
 	"open",
 	"help",
 }
@@ -43,15 +47,248 @@ func main() {
 
 		// add checks for supported singular commands here
 
+		// check against reserved stuff
+		for _, e := range reservedPrefixes {
+			if e == os.Args[0] {
+				panic("reserved") //TODO: do this better
+			}
+		}
+
 		CreateCommand(os.Args[0])
+		return
 	}
 
 	// TODO: subcommand tree
 	switch os.Args[0] {
+	case "branch":
+		shift(&os.Args)
+		if len(os.Args) == 0 {
+			panic("TODO: implement usage: need to pass parent id")
+		}
+
+		BranchCommand(os.Args[0])
 	}
 }
 
 // TODO: branch command
+func BranchCommand(parentId string) {
+
+	// NOTE: how to make sure that the file names in the system and the links
+	// are always in sync?
+	//	- normally, zets are write-only, except for renaming and extraction and
+	//	possible corruption/deletion
+	//	- if the zettel is master, the functions for modification must account
+	//	for updating no-longer-valid links
+	//	- if the file system is master, the situation is more unknown, and
+	//	responsibilities aren't clear
+	//	- therefore the zettel should be master
+
+	fileName := fmt.Sprintf("%s.md", parentId)
+	filePath := path.Join(zetDir, fileName)
+
+	// read in the file
+	byteContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Unable to open parent '%s' for branching: %s", filePath, err)
+	}
+
+	content := string(byteContent)
+	links := ExtractLinksFromContent(content)
+	branches := FilterBranches(links, parentId)
+	next, err := NextBranch(branches)
+	if err != nil {
+		log.Fatalf("Unable to calculate next branch: %s", err)
+	}
+
+	branchId := parentId + next
+
+	createZettelFile(branchId + "1") // start branches on sequence no. 1
+
+	// output branch link
+	fmt.Printf("[[%s]]\n", branchId)
+}
+
+func fileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return err == nil || !os.IsNotExist(err)
+}
+
+func createZettelFile(zettelId string) string {
+	fileName := fmt.Sprintf("%s.md", zettelId)
+	filePath := path.Join(zetDir, fileName)
+
+	if fileExists(filePath) {
+		log.Fatalf("Attempted to create existing file: %s", filePath)
+	}
+
+	f, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalf("Unable to create file '%s': %s", filePath, err)
+	}
+	ts := timestamp()
+	content := fmt.Sprintf("---\nzettel: %s\ndate: %s\n---\n\n\n\n", zettelId, ts)
+	f.Write([]byte(content))
+	f.Close()
+	return filePath
+}
+
+// ExtractLinksFromContent takes the entire content of a zettel and extracts
+// all links from it, stripping them of markup, leaving only the linked zettel
+// IDs as a string slice.
+func ExtractLinksFromContent(content string) []string {
+	var links []string
+
+	r := regexp.MustCompile(`\[\[(?P<link>[a-zA-Z0-9\.\-\_]+)\]\]`)
+
+	for _, line := range strings.Split(content, "\n") {
+
+		match := r.FindStringSubmatch(line)
+		if len(match) < 2 {
+			continue
+		}
+
+		links = append(links, match[1])
+
+	}
+
+	return links
+}
+
+// FilterBranches takes a slice of links (as stripped zettel IDs) and a zettel
+// ID, and filters out all links that are not direct branches of the zettel ID.
+func FilterBranches(links []string, parentId string) []string {
+	var branches []string
+
+	split := strings.Split(parentId, ".")
+
+	// Branches are always alphabetically suffixed. links to specific zettels
+	// in a branch have the sequence number
+	r := regexp.MustCompile(
+		fmt.Sprintf(`%v\.(?P<branch>%v[a-z]+$)`, split[0], split[1]),
+	)
+
+	for _, l := range links {
+		match := r.FindStringSubmatch(l)
+		if len(match) > 1 {
+			branch := fmt.Sprintf("%v.%v", split[0], match[1])
+			branches = append(branches, branch)
+		}
+	}
+
+	return branches
+}
+
+// NextBranch takes a list of sibling zettel IDs and returns the ID of the next
+// upcoming branch on the parent zettel, as well as an error. In the case of an
+// empty slice (no other children of current zettel), returns an 'a'.
+func NextBranch(branches []string) (string, error) {
+
+	// the first branch will always be 'a' in a numbered sceme
+	if len(branches) == 0 {
+		return "a", nil
+	}
+
+	var err error
+
+	var isDigits bool
+	maxChars := ""
+	for _, branch := range branches {
+		_, branch, isDigits, err = StripLeaf(branch)
+		if err != nil {
+			return "", fmt.Errorf("unable to strip branch leaf: %w", err)
+		}
+
+		// TODO: clean this up
+		if isDigits {
+			panic("Attempting to process branch that ends in a number. This is no longer applicable, and there must have happened a programming error. This is a bug.")
+		} else {
+			maxChars, err = AlphaMax(maxChars, branch)
+			if err != nil {
+				return "", err
+			}
+		}
+	}
+
+	if maxChars == "" {
+		panic("highest branch number not detected after iterating through all given branches in NextBranch")
+	}
+
+	nextAlphaBranch, err := IncrementAlphaBranch(maxChars)
+	if err != nil {
+		return "", fmt.Errorf("unable to increment leaf: %w", err)
+	}
+	return nextAlphaBranch, nil
+}
+
+// StripLeaf takes a zettel ID and strips the leaf branch off it, splitting the
+// ID into its parent and child components.
+//
+// E.g: tmp.12.321aa32c69 -> 'tmp.12.321aa32c', '69'
+//
+// Returns base, branch, a boolean stating if the stripped leaf was numeric or
+// not, and potentially an error.
+func StripLeaf(id string) (string, string, bool, error) {
+	var base string
+	var branch string
+	var isDigits bool
+	var err error
+
+	runes := []rune(id)
+	lastRune := runes[len(runes)-1]
+	isDigits = unicode.IsDigit(lastRune)
+
+	i := -2
+	for unicode.IsDigit(runes[len(runes)+i]) == isDigits && i+len(runes) > 0 {
+		i -= 1
+	}
+
+	base = id[:len(runes)+i+1]
+	branch = id[len(runes)+i+1:]
+
+	return base, branch, isDigits, err
+}
+
+// AlphaMax takes two alphabetic strings and returns the one with the highest
+// lexical value. Returns error if the strings are equal.
+func AlphaMax(a, b string) (string, error) {
+	if len(b) < len(a) {
+		return a, nil
+	}
+	if len(a) < len(b) {
+		return b, nil
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] > b[i] {
+			return a, nil
+		}
+		if a[i] < b[i] {
+			return b, nil
+		}
+	}
+
+	return "", errors.New(fmt.Sprintf("a and b seem to be equal: %q, %q", a, b))
+}
+
+// IncrementAlphaBranch takes a zettel ID that ends in an alphabetic character
+// and returns its zettelkasten-ID increment. E.g: tmp.1a -> tmp.1b. Returns
+// error on invalid input.
+func IncrementAlphaBranch(id string) (string, error) {
+	var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+	if id[len(id)-1:] == "z" {
+		return id + "a", nil
+	}
+
+	for i, r := range alphabet {
+		if string(r) == id[len(id)-1:] {
+			return id[:len(id)-1] + string(alphabet[i+1]), nil
+		}
+	}
+
+	return id, errors.New("invalid branch string")
+}
+
 // TODO: open command
 
 // TODO: extract command
@@ -121,18 +358,7 @@ func CreateCommand(prefix string) {
 		zettelId = fmt.Sprintf("%s%d", prefix, maxNum+1)
 	}
 
-	fileName := fmt.Sprintf("%s.md", zettelId)
-	filePath := path.Join(zetDir, fileName)
-	f, err := os.Create(filePath)
-	if err != nil {
-		log.Fatalf("Unable to create file '%s': %s", filePath, err)
-	}
-
-	ts := timestamp()
-
-	content := fmt.Sprintf("---\nzettel: %s\ndate: %s\n---\n\n\n\n", zettelId, ts)
-	f.Write([]byte(content))
-	f.Close()
+	filePath := createZettelFile(zettelId)
 
 	openInEditor(filePath)
 }
